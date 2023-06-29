04cfbf8b18ae90080b418e4533a67637f022df57
OmarZiyada1 <omarziyada16@gmail.com>
2023-06-29 15:20:20 +0200

    shit_2

---
+++ E-shop/src/ui/gui/panels/AddArtikel.java
@@ -0,0 +1,26 @@
+package ui.gui.panels;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import domain.E_Shop;
+import entities.Artikel;
+public class AddArtikel extends JPanel{
+	
+	
+	public interface AddArticleListener {
+		public void onArticleAdded(Artikel artikel);
+	}
+	
+	private E_Shop sh = null;
+	private AddArticleListener addBookListener = null;
+
+	private JButton hinzufuegenButton;
+	private JTextField nameTextFeld = null;
+	private JTextField beschreibungTextFeld = null;
+	private JTextField preis = null;
+	private JTextField bestand = null;
+	
+	
+
+}
